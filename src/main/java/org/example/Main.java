48d2908aeb05a19fbc863ac74428f9597d1e1e4f
prabhat2505 <prabhat.rana828@gmail.com>
2023-11-09 17:05:40 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.java.io.TextInputFormat;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.file.src.FileSource;
 import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
@@ -12,23 +11,32 @@ import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.example.pojo.Employee;
-import org.springframework.beans.factory.annotation.Value;
+
+
+import java.io.FileInputStream;
+
+import java.util.Properties;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    @Value("${spring.kafka.topic.name}")
-    public static String topicName;
+    private static String bootstarpServer;
+    private static String topic;
     public static void main(String[] args) throws Exception {
-
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src/main/resources/application.properties"));
+        bootstarpServer = properties.getProperty("spring.kafka.producer.bootstrap-servers");
+        topic = properties.getProperty("spring.kafka.topic.name");
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         String path = "src\\main\\resources\\file";
+
+        System.out.println(properties.getProperty("spring.kafka.producer.bootstrap-servers"));
+
 
           // Deprecated approach
 //        TextInputFormat textInputFormat = new TextInputFormat(new Path(path));
 //        DataStream<String> stream = env.readFile(textInputFormat,path);
-//        System.out.println(topicName);
+
 
         final FileSource<String> source =
                 FileSource.forRecordStreamFormat(new TextLineInputFormat(),new Path(path))
@@ -37,17 +45,17 @@ public class Main {
         env.fromSource(source, WatermarkStrategy.noWatermarks(), "file-source");
         stream.print();
 
-//        KafkaSink<String> sink = KafkaSink.<String>builder()
-//                .setBootstrapServers("localhost:9092")
-//                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
-//                        .setTopic("device_info")
-//                        .setValueSerializationSchema(new SimpleStringSchema())
-//                        .build()
-//                )
-//                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
-//
-//                .build();
-//        stream.sinkTo(sink);
+        KafkaSink<String> sink = KafkaSink.<String>builder()
+                .setBootstrapServers(bootstarpServer)
+                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setTopic(topic)
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build()
+                )
+                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+
+                .build();
+        stream.sinkTo(sink);
         env.execute();
     }
 }
